c08c9f3583dd60916c407c7e32179a4e058f9a56
Boyan Yotov <bojan.jotov@gmail.com>
2026-08-11 16:46:24 +0300

    Update 01_Count_Employees_by_Town.sql

--- Database_Programmability/Lab/01_Count_Employees_by_Town.sql
+++ Database_Programmability/Lab/01_Count_Employees_by_Town.sql
@@ -1,1 +1,23 @@
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20))
+RETURNS int
+AS
+    $$
+    DECLARE
+      e_count int;
 
+    BEGIN
+    SELECT COUNT(employee_id) FROM employees
+        JOIN 
+          addresses 
+          USING (address_id)
+        JOIN towns 
+          ON addresses.town_id = towns.town_id
+        WHERE 
+          towns.name = town_name
+        INTO 
+          e_count;
+    RETURN e_count;
+    END;
+    $$
+
+LANGUAGE plpgsql;
