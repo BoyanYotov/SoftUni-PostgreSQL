7c53bd32faae1c4ce551e5ecf212351039cf0959
Boyan Yotov <bojan.jotov@gmail.com>
2026-08-11 16:40:03 +0300

    Update 01_Towns_Addresses.sql

--- Subqueries_&_JOINs/Lab/01_Towns_Addresses.sql
+++ Subqueries_&_JOINs/Lab/01_Towns_Addresses.sql
@@ -1,1 +1,14 @@
-
+SELECT
+    towns.town_id,
+    name AS town_name,
+    address_text
+FROM
+  addresses
+JOIN
+  towns
+ON 
+  addresses.town_id = towns.town_id
+WHERE
+  name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY
+  town_id, address_id;
